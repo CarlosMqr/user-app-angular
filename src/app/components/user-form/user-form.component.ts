4a7feca175a2ab200d4ce9faefefe92f837385f7
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2026-06-25 14:57:53 -0600

    se agrego el archivo user-fomr.component .html y .ts

---
+++ src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [],
+  templateUrl: './user-form.component.html'
+})
+export class UserFormComponent {}
